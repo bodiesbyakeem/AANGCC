9a6e0a7f71436bf63aac4450a7bbdb10e242f9c1
Akeem Disu <bodiesbyakeem@gmail.com>
2026-04-01 09:47:44 -0500

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,1 +1,23 @@
+import { notFound } from "next/navigation";
+import { BLOG_POSTS } from "../page";
+import BlogPostClient from "./BlogPostClient";
+
+export async function generateStaticParams() {
+  return BLOG_POSTS.map((post) => ({ slug: post.slug }));
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const post = BLOG_POSTS.find((p) => p.slug === params.slug);
+  if (!post) return {};
+  return {
+    title: `${post.title} | AANGCC Blog`,
+    description: post.excerpt,
+  };
+}
+
+export default function BlogPostPage({ params }: { params: { slug: string } }) {
+  const post = BLOG_POSTS.find((p) => p.slug === params.slug);
+  if (!post) notFound();
+  return <BlogPostClient post={post} />;
+}
 
