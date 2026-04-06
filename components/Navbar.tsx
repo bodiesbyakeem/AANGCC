905a5e29ed23775130e5445c764aae7f4d16702e
Akeem Disu <bodiesbyakeem@gmail.com>
2026-04-06 14:58:55 -0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -40,7 +40,7 @@ const NAV_LINKS = [
     href: "#",
     dropdown: [
       { label: "Corporate Sponsorship", href: "/more/sponsorship" },
-      { label: "Donate to AANGCC", href: "/more/donate" },
+      
       { label: "Donate to AANGCC", href: "https://donate.stripe.com/7sY8wH3bgcnh0WQ6CV5AQ0g" },
       { label: "Club Rules", href: "/more/club-rules" },
       { label: "Code of Conduct", href: "/more/code-of-conduct" },
