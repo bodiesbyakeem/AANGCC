e8b89c2334e0263c8b0171e01053585429266d01
Akeem Disu <bodiesbyakeem@gmail.com>
2026-04-10 20:08:28 -0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,25 @@
+import { MetadataRoute } from "next";
+
+const SITE_URL = "https://www.allassnogascyclingclub.com";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/portal",
+          "/portal/",
+          "/admin/",
+          "/api/",
+          "/auth/",
+          "/checkin/",
+        ],
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
+}
 
